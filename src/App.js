836cc00373907baef7cc38ad9d6778fd89d5c409
tuvieja <prueba@correo.com>
2023-02-12 21:09:26 -0500

    cambiando el js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with dieguitoxd
+          Learn React with diego nina palacios
         </a>
       </header>
     </div>
