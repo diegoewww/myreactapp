0126dff69514fec6a43d2ea385b0c0b2bce52605
tuvieja <prueba@correo.com>
2023-02-12 22:22:28 -0500

    cambiando app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with JOYIT
+          Learn React with JOYIT DIEGO NINA PALACIOS
         </a>
       </header>
     </div>
