b9318ca2bd202ab12de3d31aae577310d841561b
tuvieja <prueba@correo.com>
2023-02-12 22:03:10 -0500

    comiteando

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with DiegoNina
+          Learn React with JOYIT
         </a>
       </header>
     </div>
