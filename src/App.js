56a11f3a94cb65b2f7e18320f56113081a75248a
nina1666 <105611148+nina1666@users.noreply.github.com>
2023-02-12 20:21:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with diegonina
+          Learn React
         </a>
       </header>
     </div>
