ef72165f76bbb3efdd3094bd6390f45cd51ac01f
nina1666 <105611148+nina1666@users.noreply.github.com>
2023-02-12 19:56:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React a
+          Learn react USANDO GITHUB ACTIONS
         </a>
       </header>
     </div>
